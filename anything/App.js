553c752ae29ce404bd199c011772e3e7bf2459b0
jooneei17 <82086303+jooneei17@users.noreply.github.com>
2022-07-09 01:22:04 +0900

    Add files via upload
    네이버 페이지로 넘어가는 코드

--- anything/App.js
+++ anything/App.js
@@ -1,25 +1,39 @@
+// import React from 'react';
+// function App() {
+  
+//   let openLink = () => {
+//     let pageUrl = 'http://www.naver.com';
+//     document.href = pageUrl;
+//   }
+//     return  (
+//         <div>
+//             <a href="javascript:void(0);" onClick={() => window.open([openLink], 'blank')}>
+//                 link click
+//             </a>
+//         </div>
+//     );
+// }
+ 
+// export default App;
+
+
 import React from 'react';
-import { Grid } from '@material-ui/core';
-import ExplainBox from './components/ExplainBox';
-import IDBox from './components/IDBox';
-import JoinBox from './components/JoinBox';
-
-function App() {
-    return (
-        <Grid
-            container
-            direction="column"
-            alignItems="stretch">
-
-            <ExplainBox></ExplainBox>
-            <IDBox></IDBox>
-            <JoinBox login="로그인" sign_in="회원가입"
-            onChangeMode={function(){
-
-            }}>
-            </JoinBox>
-        </Grid>
+ 
+function App () {
+    let openLink = () => {
+        let pageUrl = 'http://www.naver.com';
+        document.location.href = pageUrl;
+    }
+ 
     
-  )  
+    return  (
+        <div>
+            <a href="javascript:void(0);" onClick={openLink}>
+                link click
+            </a>
+        </div>
+    );
+  
 }
+ 
 export default App;
