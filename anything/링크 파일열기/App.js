d99cb8eb2a8812d7a0031078412ce84f8d4f7692
jooneei17 <82086303+jooneei17@users.noreply.github.com>
2022-07-12 16:00:40 +0900

    Add files via upload

---
+++ anything/링크 파일열기/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+function App() {
+  
+  let openLink = () => {
+    let pageUrl 
+    document.href = pageUrl;
+  }
+    return  (
+        <div>
+            <a href="javascript:void(0);" onClick={() => window.open([openLink], 'blank')}>
+                link click
+            </a>
+        </div>
+    );
+}
+
+export default App;
+
+
+// import React from 'react';
+ 
+// function App () {
+//     let openLink = () => {
+//         let pageUrl = 'http://www.naver.com';
+//         document.location.href = pageUrl;
+//     }
+ 
+    
+//     return  (
+//         <div>
+//             <a href="javascript:void(0);" onClick={openLink}>
+//                 link click
+//             </a>
+//         </div>
+//     );
+  
+// }
+
+// export default App;
